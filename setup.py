8799111486b7d9984d676fa360f2f9fbd21e6a69
Tam Nguyen <Tam_Nguyen@McKinsey.com>
2020-06-16 21:24:40 +0800

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kedro-great",
-    version="0.2.0",
+    version="0.2.1",
     url="https://github.com/tamsanh/kedro-great.git",
     author="Tam-Sanh Nguyen",
     author_email="tamsanh@gmail.com",
